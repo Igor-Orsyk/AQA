788bd185f87e5a8333f5bd67342473c3154f450a
igor.orsyk <igor.orsyk@ringteam.com>
2020-03-11 10:03:48 +0200

    minor things fixed

--- src/hw_4_students/Student.java
+++ src/hw_4_students/Student.java
@@ -18,19 +18,13 @@ public class Student {
 
         changeRating(the_first, 8);
 
-        System.out.println("Avarage rate of the students is " + (the_first.rate + the_second.rate + the_third.rate) / 3);
-
-    }
+        System.out.println("Avarage rate of the students is " + (the_first.rate + the_second.rate + the_third.rate) / 3);}
 
     public Student(String name, int rate) {
         this.name = name;
-        this.rate = rate;
-    }
+        this.rate = rate;}
 
-    public Student() {
-    }
-
-
+    public Student() {}
 
     public String getName() {
         return name;
@@ -58,19 +52,11 @@ public class Student {
         } else {
             System.out.println("Students rate is the same, but first is first");
             return true;
-        }
-    }
+        }}
 
     public String toString(Student student){
-        return "Student name is " + student.name + ", and his rate is " + student.rate;
-    }
+        return "Student name is " + student.name + ", and his rate is " + student.rate;}
 
     public static void changeRating(Student student, int new_rate){
-        student.setRate(new_rate);
-
-    }
-
-
-
-
+        student.setRate(new_rate);}
 }
